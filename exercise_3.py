ef058fa029a06687ce67aba9c99701998836d5cd
Saahil <saahilrarora@gmail.com>
2026-02-12 15:11:04 -0800

    changed readme, added exercise 3

--- exercise_3.py
+++ exercise_3.py
@@ -1,4 +1,50 @@
 '''
-Deep Learning with Pytorch 2 for pytorch fundamentals -- tensor operations
+Deep Learning with Pytorch 2 for pytorch fundamentals -- tensor math operations
 https://www.youtube.com/watch?v=Ta3z9vZaoMc&list=PLCC34OHNcOtpcgR9LEYSdi9r7XIbpkpK1&index=4
 '''
+import torch
+import numpy as np
+
+tensor_a = torch.tensor([1,2,3,4])
+tensor_b = torch.tensor([5,6,7,8])
+
+# element wise addition
+print(tensor_a + tensor_b)
+
+# addition using torch
+print(torch.add(tensor_a, tensor_b))
+
+#subtraction
+print(tensor_b - tensor_a)
+
+# subtraction using torch
+print(torch.sub(tensor_b, tensor_a))
+
+# Multiplcation
+print(tensor_a*tensor_b)
+
+# mult using torch
+print(torch.mul(tensor_a, tensor_b))
+
+# Division
+print(tensor_b / tensor_a)
+
+# division using torch
+print(torch.div(tensor_b, tensor_a))
+
+# modulo
+print(tensor_b%tensor_a)
+
+# modulo using torch
+print(torch.remainder(tensor_b, tensor_a))
+
+# Exponents / power
+print(tensor_a ** tensor_b)
+
+# exponents using torch
+print(torch.pow(tensor_a, tensor_b))
+
+# reassignment
+# tensor_a = tensor_a+tensor_b
+tensor_a.add_(tensor_b)
+print(tensor_a)
